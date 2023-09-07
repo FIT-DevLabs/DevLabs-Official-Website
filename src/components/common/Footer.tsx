5f90c3eab83df8833b10c2b826f2197762396969
Senuri Inakshi <109761408+InakshiSenuri@users.noreply.github.com>
2023-09-07 11:51:39 +0530

    add list stylings

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 
 function ListComponent({title , items} ){
   return(
-    <div>
+    <div className="font-sans p-4 flex justify-center items-center">
       <div>
-        <h2 className="text-2xl">{title}</h2>
+        <div><h2 className="text-2xl">{title}</h2></div>
+        <div className=" bg-orange-400 h-1 w-full"></div>
       </div>
+      
       <div>
         {items.map((item,index) =>(
           <div className="flex flex-row">
-            <div className="flex items-center justify-center"><Image width={7} height = {7}  src="/Ellipse 5.png" ></Image></div>
-            <div><li className="list-none text-xl" key={index}>{item}</li></div>
+            <div className="flex items-center justify-center"><Image alt = "Bullet" width={7} height = {7}  src="/Ellipse 5.png" ></Image></div>
+            <div><li className="list-none text-xl leading-loose" key={index}>{item}</li></div>
           </div>
         ))}
       </div>
@@ -31,12 +33,12 @@ export default function Footer() {
 
   return (
     <div className="bg-black flex flex-col w-full h-[550px] font-sans text-white px-[68px] py-[20px] ">
-      <div className="flex flex-row">
-        <div className="flex flex-col">
+      <div className="flex flex-row mb-16 justify-between">
+        <div className="flex flex-col ">
           <div>
-          <Image width={250} height = {250}  src="/devlabs_logo.png"></Image>
+          <Image alt ="DevLabs LOGO" width={250} height = {250}  src="/devlabs_logo.png"></Image>
           </div>
-          <div className="w-[338px] h-[130px]">
+          <div className="w-[338px]">
           <p className="text-sm font-normal text-justify ">Empowering Businesses with Complete IT Solutions. Discover the Possibilities at DevLabs.
              Unleash the potential of technology with our comprehensive software services, ranging from development to seamless integration.
              Your success, our commitment.
@@ -44,10 +46,12 @@ export default function Footer() {
           </div>
           
         </div>
-        <div>
-          <ListComponent title = 'Pages' items={PagesList}/>
+        <div className="flex flex-row justify-around items-center">
+
+          <div><ListComponent title = 'Pages' items={PagesList}/></div>
+          <div><ListComponent title = 'Services' items={ServiceList}/></div>
         </div>
-        <div><ListComponent title = 'Services' items={ServiceList}/></div>
+        
       </div>
       <div className="h-1 w-full flex justify-center items-center  bg-orange-400"></div>
       <div></div>
