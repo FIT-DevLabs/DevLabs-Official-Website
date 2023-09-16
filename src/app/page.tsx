c615ad545231a0f55ee3572ea94fe820b509b770
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2023-09-16 23:54:42 +0530

    updatedSponsors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import React from "react";
+import { useEffect } from "react";
+import "../styles/heroStyles.css";
+
 export default function Home() {
   return (
-    <div>Home Page</div>
-  )
+    <div>
+      <div className="heroSection">
+        <div className="heroSubSection1">
+          <h1 className="heroText1">WHERE IDEAS BECOME SOFTWARE</h1>
+          <p className="heroText2">
+            Our expertise at DevLabs lies in translating imaginative ideas into
+            functional software solutions. Join us in shaping a digital future
+            that transforms concepts into reality.
+          </p>
+          <button className="heroBtn">
+            <span>Discover</span>
+          </button>
+        </div>
+      </div>
+      <script></script>
+    </div>
+  );
 }
---
+++ src/components/home/ParticleConfig.tsx
@@ -0,0 +1,24 @@
+// ParticleConfig.tsx
+import React from 'react';
+
+const particleConfig = {
+  particles: {
+    number: {
+      value: 100, // Adjust the number of particles
+    },
+    shape: {
+      type: "polygon",
+      polygon: {
+        nb_sides: 6, // Use a hexagon shape
+      },
+    },
+    size: {
+      value: 20, // Adjust the size of particles
+    },
+    // Other particle properties like color, opacity, etc.
+  },
+};
+
+export default particleConfig;
+
+
