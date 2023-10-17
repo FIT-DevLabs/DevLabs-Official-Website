d575f94ae0e0501a0881a3e2dc66b05ebc9b9a01
Senuri Inakshi <109761408+InakshiSenuri@users.noreply.github.com>
2023-10-17 21:57:26 +0530

    Update ServicesSection.tsx

--- src/components/home/ServicesSection.tsx
+++ src/components/home/ServicesSection.tsx
@@ -1,43 +1,54 @@
 import Image from "next/image"
 import Button from "../common/Button"
 
-export default function Services() {
+interface Service {
+    id: number;
+    title: string;
+    path: string;
+  }
+  
+  interface ServiceElementProps {
+    items: Service[];
+  }
 
-    function ServiceElement ({items}){
-        return(
-            <div className="flex flex-wrap ">
-                {items.map((item)=>(
-                <div className="flex flex-row  sm:w-1/2 w-full h-[115px] lg:h-[180px] lg:px-[20px] px-[10px] sm:py-[10px] py-[5px] ">
-                <div className="flex items-center justify-center bg-gray-100 w-full rounded-2xl ">
-                    <div className="w-1/3 lg:px-8 px-6">
-                       <div className="lg:w-[100px] w-[50px] lg:h-[100px] h-[50px] relative"> 
-                       <Image key={item.id} alt = "Service icon" src ={item.path} layout="fill" objectFit="contain"/></div>
-                    </div>
-                    <div key={item.id} className="flex align-left w-2/3 text-sm sm:text-base lg:text-xl font-medium ">{item.title}</div>
+function ServiceElement ({items}:ServiceElementProps){
+    return(
+        <div className="flex flex-wrap ">
+            {items.map((item)=>(
+            <div className="flex flex-row  sm:w-1/2 w-full h-[115px] lg:h-[180px] lg:px-[20px] px-[10px] sm:py-[10px] py-[5px] ">
+            <div className="flex items-center justify-center bg-gray-100 w-full rounded-2xl ">
+                <div className="w-1/3 lg:px-8 px-6">
+                   <div className="lg:w-[100px] w-[50px] lg:h-[100px] h-[50px] relative"> 
+                   <Image  alt = "Service icon" src ={item.path} layout="fill" objectFit="contain"/></div>
                 </div>
-                
-                </div>
-
-                ))}
+                <div key={item.id} className="flex align-left w-2/3 text-sm sm:text-base lg:text-xl font-medium ">{item.title}</div>
             </div>
             
-            
-        )
-    }
-    const ListofServices = [
-        {
-            id:1,title:"Software Engineering Solutions",path:"/Images/services/Icon1.png"
-        },
-        {
-            id:2,title:"Data Science, AI and ML Service",path:"/Images/services/Icon1.png"
-        },
-        {
-            id:3,title:"IoT and IoB",path:"/Images/services/Icon1.png"
-        },
-        {
-            id:4,title:"Data Science, AI and ML Service",path:"/Images/services/Icon1.png"
-        },
-    ]
+            </div>
+
+            ))}
+        </div>
+        
+        
+    )
+}
+const ListofServices = [
+    {
+        id:1,title:"Software Engineering Solutions",path:"/Images/services/Icon1.png"
+    },
+    {
+        id:2,title:"Data Science, AI and ML Service",path:"/Images/services/Icon1.png"
+    },
+    {
+        id:3,title:"IoT and IoB",path:"/Images/services/Icon1.png"
+    },
+    {
+        id:4,title:"Data Science, AI and ML Service",path:"/Images/services/Icon1.png"
+    },
+]
+
+
+export default function Services() {
 
     return (
       <div className="lg:mx-[100px] sm:mx-[50px] mx-[10px] flex flex-col rounded-t-[60px] bg-white">
