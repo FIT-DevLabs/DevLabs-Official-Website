cfb1bf31c8c7a4e4e5801d1cfede8ceb23fec081
Pramoth Dilshan <pramothdilshan2001@gmail.com>
2024-01-13 01:15:55 +0530

    feat : create story section and carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from '@/components/common/NavBar'
-import Footer from '@/components/common/Footer'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+// import NavBar from "@/components/common/NavBar";
+import Footer from "@/components/common/Footer";
+import StorySectionCarousel from "@/components/home/StorySectionCarousel";
+import StorySection from "@/components/home/StorySection";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'DevLabs',
-  description: 'DevLabs Official Website',
-}
+  title: "DevLabs",
+  description: "DevLabs Official Website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
         {children}
+        <StorySection />
         <Footer />
       </body>
     </html>
-  )
+  );
 }
